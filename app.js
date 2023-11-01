8faf0ea7974164baf30618c8f8193e9fcded97b2
Danilo Lopes <danilolopespadilha@gmail.com>
2023-11-01 14:58:06 -0300

    criando a pagina de contato

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("O Paulo fez esse commit sem voce ver na aula")
+console.log("Enviando para o Gitgub")
+console.log("Ratumba batata como é bom viver isso é voce, ratumba batata")
